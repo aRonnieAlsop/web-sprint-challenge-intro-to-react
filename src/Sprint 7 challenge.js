d14736ad92705946470ec346bc7cb5e70dc87d57
Arron Alsop <alsop.arron@gmail.com>
2023-08-05 13:12:27 -0700

    'work in progress'

--- src/Sprint 7 challenge.js
+++ src/Sprint 7 challenge.js
@@ -1,41 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
 
 const URL = 'https://swapi.dev/api/people/'
 
-const Characters = styled.div`
-    background-color: rgba(255, 255, 255, 0.5);
 
-`
-
-const Character = styled.div`
-    background-color: rgba(255, 255, 255, 0. 3);
-    border-bottom: 1px dotted rgba(0, 0, 0, 0.5);
-    font-size: 25px;
-    margin: 2px;
-    &:hover {
-      font-size: 40px;
-      text-shadow: -5px 1px brown;
-      color:  white;
-    }
-    `
-
-const Header = styled.header`
-    font-size: 60px;
-`
-
-const Subtitle = styled.h2`
-  font-family: Vazir;
-  text-shadow: 0.5px 1px brown;
-  color:  #F8F0E3;
-  font-size: 35px;
-`
-const Button = styled.button`
-  padding: 0;
-  border: none;
-  background: none;
-`
 const App = () => {
   const [chars, setChars] = useState([])
 
@@ -66,14 +34,13 @@ const App = () => {
   if (!chars) return `The force isn't strong with this one...`
   return (
     <div className="App">
-      <Header className="Header">Star Wars</Header>
-      <Subtitle>Main Characters</Subtitle>
-      <Characters>
-
+      <h1 className="Header">Star Wars</h1>
+      <h2>Main Characters</h2>
+      <section>
       {
-        chars.map(ch => <Character key={ch.id}> {ch.name}</Character>)
+        chars.map(ch => <div key={ch.id}> {ch.name}  <div>Born:  {ch.birth_year}</div></div>)
       }
-      </Characters>
+      </section>
      </div> 
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,9 @@
+import React from 'react'
+
 // Write your Character component here
+
+export default function Character({ name, birth_year }) {
+    return (
+       
+    )
+}
